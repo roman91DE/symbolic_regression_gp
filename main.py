98e177a4602b53dcd0f3a3450be62b04a0b836ad
rmn@mbp <rohoehn123@gmail.com>
2022-02-16 23:51:24 +0100

    need to structure in different files

--- main.py
+++ main.py
@@ -4,27 +4,15 @@ from matplotlib import pyplot as plt
 import random
 
 
-
-conf = {
-    "Operators": [
-        lambda a,b : a+b,
-        lambda a,b : a-b,
-        lambda a,b : a*b,
-        lambda a,b : a/b if b != 0 else 1000000000,
-        lambda a,b : a**b 
-    ],
-    "Terminals": ["x"]
-}
-
 class Node:
-    def __init__(self, config) -> None:
+    def __init__(self, config:typing.Dict) -> None:
 
-        if random.random() > .5:    
+        if random.random() > 0.5:
             # pick operator
             self.value = random.choice(config["Operators"])
             self.leftchild = Node(config)
             self.righchild = Node(config)
-        else:                       
+        else:
             # pick terminal
             self.value = random.choice(config["Terminals"])
             self.leftchild = None
@@ -35,7 +23,6 @@ class Program:
     def __init__(self, config: typing.Dict) -> None:
         self.config = config
         self.root = Node(self.config)
-
 
 
 class Testcase:
@@ -56,12 +43,22 @@ class Testcase:
         ax.scatter(self.X, self.Y)
         fig.savefig(f"{filename}.png")
 
-    def compute_fitness(program:Program):
+    def compute_fitness(program: Program):
         pass
 
 
-
-
 if __name__ == "__main__":
+
+    conf = {
+        "Operators": [
+            lambda a, b: a + b,
+            lambda a, b: a - b,
+            lambda a, b: a * b,
+            lambda a, b: a / b if b != 0 else 1000000000,
+            lambda a, b: a ** b,
+        ],
+        "Terminals": ["x"],
+    }
+
     tc = Testcase(lambda x: x * x, -10, 10, 20, 100)
     tc.draw("test.png")
